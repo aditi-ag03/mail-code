1aba2418a0d23a7e2273a972939d291b4a5faa53
shelly agarwal <64473186+aditi-ag03@users.noreply.github.com>
2020-06-23 05:01:26 +0530

    v2

--- mail.py
+++ mail.py
@@ -2,7 +2,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-email_content="hello"
+email_content='hello'
 sender_address=''
 sender_pass=''
 receiver_address=''
